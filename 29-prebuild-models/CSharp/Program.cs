cdab7a74ed029ddf150b2870df7d368dcc12b89b
aashnajoshi <aashnahetal@gmail.com>
2024-03-19 13:08:08 +0530

    .

--- 29-prebuild-models/CSharp/Program.cs
+++ 29-prebuild-models/CSharp/Program.cs
@@ -11,14 +11,26 @@ Console.WriteLine("\nConnecting to Forms Recognizer at: {0}", endpoint);
 Console.WriteLine("Analyzing invoice at: {0}\n", fileUri.ToString());
 
 // Create the client
+var cred = new AzureKeyCredential(apiKey);
+var client = new DocumentAnalysisClient(new Uri(endpoint), cred);
 
 // Analyze the invoice
+AnalyzeDocumentOperation operation = await client.AnalyzeDocumentFromUriAsync(WaitUntil.Completed, "prebuilt-invoice", fileUri);
+await operation.WaitForCompletionAsync();
 
 // Display invoice information to the user
+AnalyzeResult result = operation.Value;
 
-
-
-
+foreach (AnalyzedDocument invoice in result.Documents)
+{
+    if (invoice.Fields.TryGetValue("VendorName", out DocumentField? vendorNameField))
+    {
+        if (vendorNameField.FieldType == DocumentFieldType.String)
+        {
+            string vendorName = vendorNameField.Value.AsString();
+            Console.WriteLine($"Vendor Name: '{vendorName}', with confidence {vendorNameField.Confidence}.");
+        }
+    }
 
     if (invoice.Fields.TryGetValue("CustomerName", out DocumentField? customerNameField))
     {
--- 29-prebuild-models/Python/document-analysis.py
+++ 29-prebuild-models/Python/document-analysis.py
@@ -2,8 +2,8 @@ from azure.core.credentials import AzureKeyCredential
 from azure.ai.formrecognizer import DocumentAnalysisClient
 
 # Store connection information
-endpoint = "<Endpoint URL>"
-key = "<API Key>"
+endpoint = "YOUR_ENDPOINT"
+key = "YOUR_KEY"
 
 fileUri = "https://github.com/MicrosoftLearning/mslearn-ai-document-intelligence/blob/main/Labfiles/01-prebuild-models/sample-invoice/sample-invoice.pdf?raw=true"
 fileLocale = "en-US"
@@ -26,16 +26,16 @@ poller = document_analysis_client.begin_analyze_document_from_url(
 receipts = poller.result()
 
 for idx, receipt in enumerate(receipts.documents):
-  vendor_name = receipt.fields.get("VendorName")
-  if vendor_name:
-      print(f"\nVendor Name: {vendor_name.value}, with confidence {vendor_name.confidence}.")
+    vendor_name = receipt.fields.get("VendorName")
+    if vendor_name:
+        print(f"\nVendor Name: {vendor_name.value}, with confidence {vendor_name.confidence}.")
 
-      customer_name = receipt.fields.get("CustomerName")
-      if customer_name:
-          print(f"Customer Name: '{customer_name.value}, with confidence {customer_name.confidence}.")
+    customer_name = receipt.fields.get("CustomerName")
+    if customer_name:
+        print(f"Customer Name: '{customer_name.value}, with confidence {customer_name.confidence}.")
 
-      invoice_total = receipt.fields.get("InvoiceTotal")
-      if invoice_total:
-          print(f"Invoice Total: '{invoice_total.value.symbol}{invoice_total.value.amount}, with confidence {invoice_total.confidence}.")
+    invoice_total = receipt.fields.get("InvoiceTotal")
+    if invoice_total:
+        print(f"Invoice Total: '{invoice_total.value.symbol}{invoice_total.value.amount}, with confidence {invoice_total.confidence}.")
 
 print("\nAnalysis complete.\n")
